c5acf7d55ef213fd5eae2a798ff5e5bbe1296408
Marc-André Courtois <2489820+mcourtois@users.noreply.github.com>
2025-04-05 01:05:16 +0900

    Compile with custom git user feature

--- dist/index.js
+++ dist/index.js
@@ -29668,6 +29668,8 @@ const PULL_REQUEST_LABELS    = core.getInput( 'PULL_REQUEST_LABELS' );
 const SKIP_CI                = toolkit.input.tobool( core.getInput( 'SKIP_CI' ) );
 const GITHUB_TOKEN           = core.getInput( 'GITHUB_TOKEN' );
 const GIT_URL                = core.getInput( 'GIT_URL' );
+const GIT_USER               = core.getInput( 'GIT_USER' );
+const GIT_EMAIL              = core.getInput( 'GIT_EMAIL' );
 const RAW_REPOSITORIES       = core.getInput( 'REPOSITORIES' );
 const COMMIT_MESSAGE         = core.getInput( 'COMMIT_MESSAGE' );
 const RAW_WORKFLOW_FILES     = core.getInput( 'WORKFLOW_FILES' );
@@ -29679,8 +29681,8 @@ const GITHUB_WORKSPACE       = toolkit.input.env( 'GITHUB_WORKSPACE' );
 const WORKSPACE              = toolkit.path.dirname( toolkit.path.dirname( GITHUB_WORKSPACE ) ) + '/workflow-sync/';
 
 module.exports = {
-	GIT_USER: 'Workflow Sync Bot',
-	GIT_EMAIL: 'githubactionbot+workflowsync@gmail.com',
+	GIT_USER,
+	GIT_EMAIL,
 	AUTO_CREATE_NEW_BRANCH,
 	COMMIT_EACH_FILE,
 	DRY_RUN,
